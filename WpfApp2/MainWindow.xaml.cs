6a8ad23b5636f7d7f45987224dfaf5389210f38c
KevinChena <z6256875@gmail.com>
2023-09-23 10:55:41 +0800

    新增drinks order兩個Dic資料結構，建立飲料訂購GUI

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -19,10 +19,43 @@ namespace WpfApp2
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {   
+
+        Dictionary<string, int>  drinks = new Dictionary<string, int>();
+        Dictionary<string, int>  order = new Dictionary<string, int>();
         public MainWindow()
-        {
+        {   
             InitializeComponent();
+            AddNewDrink(drinks);
+        }
+
+        private void AddNewDrink(Dictionary<string, int> mydrinks)
+        {
+            mydrinks.Add("紅茶大杯", 60);
+            mydrinks.Add("紅茶小杯", 40);
+            mydrinks.Add("綠茶大杯", 60);
+            mydrinks.Add("綠茶小杯", 40);
+            mydrinks.Add("青茶大杯", 60);
+            mydrinks.Add("青茶小杯", 40);
+            mydrinks.Add("咖啡大杯", 80);
+            mydrinks.Add("咖啡小杯", 60);
+        }
+
+        private void PlaceOrder(object sender, TextChangedEventArgs e)
+        {
+            var targetTextBox = sender as TextBox;
+            bool success = int.TryParse(targetTextBox.Text, out int amount);
+            if (!success)MessageBox.Show("請輸入整數", "輸入錯誤");
+            else if (amount <= 0) MessageBox.Show("輸入數值必須大於0", "輸入錯誤");
+            else
+            {
+                StackPanel targetPanel = targetTextBox.Parent as StackPanel;
+                Label targetLabel = targetPanel.Children[0] as Label;
+                string drinkName = targetLabel.Content.ToString();
+
+                if (order.ContainsKey(drinkName)) order.Remove(drinkName);
+                order.Add(drinkName, amount);
+            }
         }
     }
 }
