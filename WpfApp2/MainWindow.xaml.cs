21a961a31d73433a095e7232b7f77620400279cf
KevinChena <z6256875@gmail.com>
2023-09-25 09:11:24 +0800

    整合訂單整理以及顯示介面

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -46,7 +46,7 @@ namespace WpfApp2
             var targetTextBox = sender as TextBox;
             bool success = int.TryParse(targetTextBox.Text, out int amount);
             if (!success)MessageBox.Show("請輸入整數", "輸入錯誤");
-            else if (amount <= 0) MessageBox.Show("輸入數值必須大於0", "輸入錯誤");
+            else if (amount < 0) MessageBox.Show("輸入數值必須大於0", "輸入錯誤");
             else
             {
                 StackPanel targetPanel = targetTextBox.Parent as StackPanel;
@@ -56,6 +56,50 @@ namespace WpfApp2
                 if (order.ContainsKey(drinkName)) order.Remove(drinkName);
                 order.Add(drinkName, amount);
             }
+        }
+        private void Click_botton(object sender, RoutedEventArgs e)
+        {
+            double total= 0.0;
+            double sellPrice= 0.0;
+            string message = "";
+            string displaystring = "訂購清單如下:\n";
+            
+            foreach (var item in order)
+            {
+                string drinkName = item.Key;
+                int quantity = order[drinkName];
+                int price = drinks[drinkName];
+
+                total += quantity * price;
+                displaystring += $"{drinkName}   {quantity} 杯， 每杯{price} 共{quantity*price}元\n";
+            }
+
+            if (total >= 500)
+            {
+                sellPrice = total * 0.8;
+                message = "訂購500元以上者8折";
+
+            }
+            else if (total >= 300)
+            {
+                sellPrice = total * 0.9;
+                message = "訂購300元以上者9折";
+            }
+            else if (total >= 200)
+            {
+                sellPrice = total * 0.95;
+                message = "訂購200元以上者95折";
+            }
+            else
+            {
+                sellPrice = total;
+                message = "訂購未滿200元不打折";
+            }
+
+            displaystring += $"{message}\n原訂單總價為{total}元，折扣後為{sellPrice}元";
+            textblock.Text = displaystring;
+
+
         }
     }
 }
