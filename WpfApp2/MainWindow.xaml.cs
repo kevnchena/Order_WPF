9ed69e21d9a05c222725568872cba953d3856614
KevinChena <z6256875@gmail.com>
2023-10-02 21:34:57 +0800

    新增存檔輸出txt csv功能

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Security.Permissions;
+
 
 namespace WpfApp2
 {
@@ -21,14 +23,14 @@ namespace WpfApp2
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {   
+    {
 
-        Dictionary<string, int>  drinks = new Dictionary<string, int>();
-        Dictionary<string, int>  orders = new Dictionary<string, int>();
-        string takeout="";
+        Dictionary<string, int> drinks = new Dictionary<string, int>();
+        Dictionary<string, int> orders = new Dictionary<string, int>();
+        string takeout = "";
 
         public MainWindow()
-        {   
+        {
             InitializeComponent();
             //新增飲料
             AddNewDrink(drinks);
@@ -39,7 +41,7 @@ namespace WpfApp2
 
         private void DisplayDrinkMenu(Dictionary<string, int> mydrinks)
         {
-            foreach(var drink in mydrinks)
+            foreach (var drink in mydrinks)
             {
 
                 CheckBox cb = new CheckBox
@@ -75,7 +77,7 @@ namespace WpfApp2
                 };
                 sp.Children.Add(cb);
                 sp.Children.Add(sl);
-                sp.Children.Add(lb); 
+                sp.Children.Add(lb);
 
                 Binding myBinding = new Binding("Value");
                 myBinding.Source = sl;
@@ -85,7 +87,7 @@ namespace WpfApp2
 
 
                 DrinkMenu.Children.Add(sp);
-                DrinkMenu.Height = (mydrinks.Count+1)*35;
+                DrinkMenu.Height = (mydrinks.Count + 1) * 35;
             }
 
 
@@ -123,7 +125,7 @@ namespace WpfApp2
         {
             var targetTextBox = sender as TextBox;
             bool success = int.TryParse(targetTextBox.Text, out int amount);
-            if (!success)MessageBox.Show("請輸入整數", "輸入錯誤");
+            if (!success) MessageBox.Show("請輸入整數", "輸入錯誤");
             else if (amount < 0) MessageBox.Show("輸入數值必須大於0", "輸入錯誤");
             else
             {
@@ -141,6 +143,8 @@ namespace WpfApp2
             OrderList(orders);
             //顯示訂單細項
             DisplayOrderDetail(orders);
+            //印出單
+            SaveOrderList(displaytextblock.Text.ToString());
 
         }
 
@@ -149,7 +153,7 @@ namespace WpfApp2
             displaytextblock.Inlines.Clear();
             Run titleString = new Run
             {
-                Text = "你所訂購的品項",
+                Text = "取貨方式: ",
                 Foreground = Brushes.Blue,
                 FontSize = 16,
                 FontWeight = FontWeights.Bold,
@@ -168,11 +172,10 @@ namespace WpfApp2
             displaytextblock.Inlines.Add(titleString);
             displaytextblock.Inlines.Add(takeoutString);
             displaytextblock.Inlines.Add(new Run("訂購明細如下:\n"));
-            
+
             double total = 0.0;
             double sellPrice = 0.0;
             int i = 1;
-
 
 
             foreach (var item in myorders)
@@ -188,60 +191,103 @@ namespace WpfApp2
                 displaytextblock.Inlines.Add(detailString);
             }
 
+            Run pricemessage = new Run();
             if (total >= 500)
             {
                 sellPrice = total * 0.8;
-                
+                pricemessage.Text = "訂購500元以上者8折\n";
+                pricemessage.Background = Brushes.Goldenrod;
+                pricemessage.FontWeight = FontWeights.Bold;
 
             }
             else if (total >= 300)
             {
                 sellPrice = total * 0.9;
-               
+                pricemessage.Text = "訂購300元以上者9折\n";
+                pricemessage.Background = Brushes.GreenYellow;
+
             }
             else if (total >= 200)
             {
                 sellPrice = total * 0.95;
-                
+                pricemessage.Text = "訂購200元以上者95折\n";
+                pricemessage.Background = Brushes.BlueViolet;
             }
             else
             {
                 sellPrice = total;
-                
+                pricemessage.Text = "未滿200不打折\n";
+                pricemessage.Background = Brushes.Gray;
             }
+
+            displaytextblock.Inlines.Add(pricemessage);
             Italic summaryString = new Italic(new Run($"本次訂購{myorders.Count}杯，原訂單總價為{total}元，折扣後為{sellPrice}元"));
 
             displaytextblock.Inlines.Add(summaryString);
 
         }
+        private void SaveOrderList(string displaytext)
+        {
+            SaveFileDialog saveOrderDialog = new SaveFileDialog();
+            Stream mystream = null;
+            saveOrderDialog.Filter = "CSV檔案|*.csv|文字檔案|*.txt";
+            saveOrderDialog.FilterIndex = 2;
+            saveOrderDialog.RestoreDirectory = true;
+            saveOrderDialog.Title = "Save an csv or txt File";
+
+            if (saveOrderDialog.ShowDialog() == true)
+            {
+                mystream = saveOrderDialog.OpenFile();
+                StreamWriter writer = new StreamWriter(mystream);
+                switch (saveOrderDialog.FilterIndex)
+                {
+                    case 1:
+                        using (writer)
+                        {
+                            writer.WriteLine(displaytext);
+                        }
+                        break;
+
+                    case 2:
+                        using (writer)
+                        {
+                            writer.WriteLine(displaytext);
+                        }
+                        break;
+                }
+
+                //mystream.Close();
+            }
+        }
+
 
         private void OrderList(Dictionary<string, int> myorders)
-        {
-           myorders.Clear();
-           for(int i = 0; i < DrinkMenu.Children.Count; i++)
             {
-                var sp = DrinkMenu.Children[i] as StackPanel;
-                var cb = sp.Children[0] as CheckBox;
-                var sl = sp.Children[1] as Slider;
-                String drinkName = cb.Content.ToString().Substring(0, 4);
-                int quantity = Convert.ToInt32(sl.Value);
-
-                if(cb.IsChecked ==true && quantity != 0)
+                myorders.Clear();
+                for (int i = 0; i < DrinkMenu.Children.Count; i++)
                 {
-                    myorders.Add(drinkName, quantity);
+                    var sp = DrinkMenu.Children[i] as StackPanel;
+                    var cb = sp.Children[0] as CheckBox;
+                    var sl = sp.Children[1] as Slider;
+                    String drinkName = cb.Content.ToString().Substring(0, 4);
+                    int quantity = Convert.ToInt32(sl.Value);
+
+                    if (cb.IsChecked == true && quantity != 0)
+                    {
+                        myorders.Add(drinkName, quantity);
+                    }
+                }
+            }
+
+            private void RadioBotton_Checked(object sender, RoutedEventArgs e)
+            {
+                var rb = sender as RadioButton;
+                if (rb.IsChecked == true)
+                {
+                    takeout = rb.Content.ToString();
+                    MessageBox.Show(takeout);
+
                 }
             }
         }
-
-        private void RadioBotton_Checked(object sender, RoutedEventArgs e)
-        {
-            var rb = sender as RadioButton;
-            if (rb.IsChecked == true)
-            {
-                takeout = rb.Content.ToString();
-                MessageBox.Show(takeout);
-                
-            }
-        }
-    }
 }
